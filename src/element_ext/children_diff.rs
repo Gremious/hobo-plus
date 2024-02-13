a094e3af04b92f59b07f853a7f6b8adc192ba6b9
Awpteamoose <awpteamoose@gmail.com>
2024-02-13 18:04:09 +0000

    making children-diff easier to use

--- src/element_ext/children_diff.rs
+++ src/element_ext/children_diff.rs
@@ -1,6 +1,40 @@
 #![allow(clippy::type_complexity)]
 
 use hobo::prelude::*;
+use hobo::{signal_map::{MapDiff, MutableBTreeMap, SignalMapExt}, signal::SignalExt};
+
+// TODO: rename
+#[derive(Clone)]
+pub struct FunnyValue<K, V> where
+	K: Ord + Clone + std::hash::Hash + 'static,
+	V: Clone + 'static,
+{
+	element: hobo::Element,
+	key: K,
+	_pd: std::marker::PhantomData<(K, V)>,
+}
+
+impl<K, V> FunnyValue<K, V> where
+	K: Ord + Clone + std::hash::Hash + 'static,
+	V: Clone + 'static,
+{
+	pub fn current(&self) -> V {
+		self.element.get_cmp::<ChildrenDiff<K, V>>().mutable.lock_ref().get(&self.key).unwrap().clone()
+	}
+
+	pub fn update(&self, f: impl FnOnce(&mut V)) {
+		let mut current = self.current();
+		f(&mut current);
+		self.element.get_cmp::<ChildrenDiff<K, V>>().mutable.lock_mut().insert_cloned(self.key.clone(), current);
+	}
+
+	// TODO: maybe instead of making a new singla every time - just use Broadcaster
+	pub fn signal(&self) -> impl hobo::signal::Signal<Item = V> + 'static {
+		self.element.get_cmp::<ChildrenDiff<K, V>>().mutable.signal_map_cloned().key_cloned(self.key.clone()).map(Option::unwrap)
+	}
+}
+
+// type KeySignal<K, V> = hobo::signal::Broadcaster<hobo::signal::Map<hobo::signal_map::MapWatchKeySignal<hobo::signal_map::MutableSignalMap<K, V>>, fn(std::option::Option<V>) -> V>>;
 
 pub struct ChildrenDiffConfig<K, V, E, Insert, OnChange, OnRemove, OnUpdate> {
 	insert: Insert,
@@ -18,8 +52,8 @@ pub struct ChildrenDiffConfigBuilder<K, V, E, Insert, OnChange, OnRemove, OnUpda
 	_pd: std::marker::PhantomData<(K, V, E)>,
 }
 
-impl<E, Insert> ChildrenDiffConfig<(), (), E, Insert, fn(), fn(&()), fn(&(), &hobo::signal::Mutable<()>)> {
-	pub fn builder<K, V>() -> ChildrenDiffConfigBuilder<K, V, E, Insert, fn(), fn(&K), fn(&K, &hobo::signal::Mutable<V>)> { ChildrenDiffConfigBuilder {
+impl<E, Insert> ChildrenDiffConfig<(), (), E, Insert, fn(), fn(&()), fn(&(), &())> {
+	pub fn builder<K, V>() -> ChildrenDiffConfigBuilder<K, V, E, Insert, fn(), fn(&K), fn(&K, &V)> { ChildrenDiffConfigBuilder {
 		insert: None,
 		on_change: move || {},
 		on_remove: move |_| {},
@@ -30,10 +64,10 @@ impl<E, Insert> ChildrenDiffConfig<(), (), E, Insert, fn(), fn(&()), fn(&(), &ho
 
 impl<K, V, E, Insert, OnChange, OnRemove, OnUpdate> ChildrenDiffConfigBuilder<K, V, E, Insert, OnChange, OnRemove, OnUpdate> where
 	E: hobo::AsElement + 'static,
-	Insert: FnMut(&K, &hobo::signal::Mutable<V>) -> E + 'static,
+	Insert: FnMut(&K, FunnyValue<K, V>) -> E + 'static,
 	OnChange: FnMut() + 'static,
 	OnRemove: FnMut(&K) + 'static,
-	OnUpdate: FnMut(&K, &hobo::signal::Mutable<V>) + 'static,
+	OnUpdate: FnMut(&K, &V) + 'static,
 {
 	#[must_use] pub fn insert(mut self, f: Insert) -> Self { self.insert = Some(f); self }
 	pub fn on_change<NewOnChange>(self, f: NewOnChange) -> ChildrenDiffConfigBuilder<K, V, E, Insert, NewOnChange, OnRemove, OnUpdate> where
@@ -55,7 +89,7 @@ impl<K, V, E, Insert, OnChange, OnRemove, OnUpdate> ChildrenDiffConfigBuilder<K,
 		_pd: std::marker::PhantomData,
 	} }
 	pub fn on_update<NewOnUpdate>(self, f: NewOnUpdate) -> ChildrenDiffConfigBuilder<K, V, E, Insert, OnChange, OnRemove, NewOnUpdate> where
-		NewOnUpdate: FnMut(&K, &hobo::signal::Mutable<V>) + 'static,
+		NewOnUpdate: FnMut(&K, &V) + 'static,
 	{ ChildrenDiffConfigBuilder {
 		insert: self.insert,
 		on_change: self.on_change,
@@ -80,7 +114,7 @@ pub struct ChildrenDiff<K, V> where
 	V: 'static,
 {
 	/// Mutable which is being updated/watched.
-	pub mutable: hobo::signal_map::MutableBTreeMap<K, hobo::signal::Mutable<V>>,
+	pub mutable: hobo::signal_map::MutableBTreeMap<K, V>,
 	/// Element which gets items appended/removed.
 	pub element: hobo::Element,
 	/// Hobo elements that represent the current state.
@@ -91,37 +125,24 @@ pub struct ChildrenDiff<K, V> where
 
 impl<K, V> ChildrenDiff<K, V> where
 	K: Ord + Clone + std::hash::Hash + 'static,
-	V: 'static,
+	V: Clone + 'static,
 {
-	pub fn add(&mut self, key: K, value: V) {
+	pub fn upsert(&mut self, key: K, value: V) {
 		let mut mutable_lock = self.mutable.lock_mut();
-		if mutable_lock.insert_cloned(key.clone(), hobo::signal::Mutable::new(value)).is_some() {
-			log::warn!("ChildrenDiff::add overriding existing value, this is likely an error");
-		}
-		self.unprocessed_ids.insert(key);
-	}
-
-	pub fn update(&mut self, key: K, value: V) {
-		let mut mutable_lock = self.mutable.lock_mut();
-		let value_mutable = mutable_lock.get(&key).unwrap().clone();
-		value_mutable.set(value);
-		// this is to trigger MapDiff::Update
-		mutable_lock.insert_cloned(key.clone(), value_mutable);
+		mutable_lock.insert_cloned(key.clone(), value);
 		self.unprocessed_ids.insert(key);
 	}
 
 	pub fn update_with(&mut self, key: K, f: impl FnOnce(&mut V)) {
 		let mut mutable_lock = self.mutable.lock_mut();
-		let value_mutable = mutable_lock.get(&key).unwrap().clone();
-		f(&mut value_mutable.lock_mut());
-		// this is to trigger MapDiff::Update
-		mutable_lock.insert_cloned(key.clone(), value_mutable);
+		let mut value = mutable_lock.get(&key).unwrap().clone();
+		f(&mut value);
+		mutable_lock.insert_cloned(key.clone(), value);
 		self.unprocessed_ids.insert(key);
 	}
 
 	pub fn remove(&mut self, key: K) {
-		let mut mutable_lock = self.mutable.lock_mut();
-		mutable_lock.remove(&key);
+		self.mutable.lock_mut().remove(&key);
 		self.unprocessed_ids.insert(key);
 	}
 }
@@ -130,23 +151,25 @@ pub trait ChildrenDiffElementExt: AsElement {
 	#[must_use]
 	fn children_diff<K, V, E, Insert, OnChange, OnRemove, OnUpdate>(self, config: ChildrenDiffConfigBuilder<K, V, E, Insert, OnChange, OnRemove, OnUpdate>) -> Self where
 		Self: Sized + Copy + 'static,
-		K: Ord + Clone + std::hash::Hash + 'static,
-		V: 'static,
+		K: Ord + Clone + std::hash::Hash + Send + 'static,
+		V: Clone + Send + 'static,
 		E: hobo::AsElement + 'static,
-		Insert: FnMut(&K, &hobo::signal::Mutable<V>) -> E + 'static,
+		Insert: FnMut(&K, FunnyValue<K, V>) -> E + 'static,
 		OnChange: FnMut() + 'static,
 		OnRemove: FnMut(&K) + 'static,
-		OnUpdate: FnMut(&K, &hobo::signal::Mutable<V>) + 'static,
+		OnUpdate: FnMut(&K, &V) + 'static,
 	{
-		use hobo::{signal_map::{MapDiff, MutableBTreeMap}, signal::Mutable};
-
 		let ChildrenDiffConfig { mut insert, mut on_change, mut on_remove, mut on_update, .. } = config.build();
-		let mutable = MutableBTreeMap::<K, Mutable<V>>::new();
+		let mutable = MutableBTreeMap::<K, V>::new();
 		self
 			.component(mutable.signal_map_cloned().subscribe(move |diff| match diff {
-				MapDiff::Insert { key, value } => {
+				MapDiff::Insert { key, .. } => {
 					{
-						let element = insert(&key, &value).as_element();
+						// let signal: hobo::signal::Map<hobo::signal_map::MapWatchKeySignal<hobo::signal_map::MutableSignalMap<K, V>>, fn(Option<V>) -> V> =
+						//     self.get_cmp::<ChildrenDiff<K, V>>().mutable.signal_map_cloned().key_cloned(key.clone()).map(Option::unwrap);
+						let funny_value = FunnyValue { element: self.as_element(), key: key.clone(), _pd: std::marker::PhantomData::<(K, V)> };
+
+						let element = insert(&key, funny_value).as_element();
 						self.add_child(element);
 
 						let mut children_diff = self.get_cmp_mut::<ChildrenDiff<K, V>>();
